73ef6e628f619e3f84c3a2be2f6c7cab0054c387
1naccessible <alexpomash@gmail.com>
2022-08-26 00:14:56 +0300

    changed sout

--- CallBeReff.java
+++ CallBeReff.java
@@ -14,9 +14,9 @@ class CallBeReffDemo{
     public static void main(String[] args) {
         CallBeReff ob = new CallBeReff(15,20);
 
-        System.out.println("ob.a and ob.b before call - " + ob.a +"/" + ob.b);
+        System.out.println("ob.a and ob.b before call: " + ob.a +"/" + ob.b);
 
         ob.change(ob);
-        System.out.println("ob.a and ob.b after call - " + ob.a + "/" + ob.b);
+        System.out.println("ob.a and ob.b after call: " + ob.a + "/" + ob.b);
     }
 }
