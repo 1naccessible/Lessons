d573afda323a7efd2e80d4b1bb79052f281abd35
1naccessible <alexpomash@gmail.com>
2022-08-26 02:50:22 +0300

    Overload Constructor Demo

---
+++ Summation.java
@@ -0,0 +1,23 @@
+public class Summation {
+    int sum;
+    //Coздать обьект на основе целочисленного значения
+    Summation(int num) {
+        sum = 0;
+        for (int i = 1; i <= num; i++)
+            sum += i;
+    }
+    //Создать обьект на основе другого
+    Summation(Summation ob) {
+        sum = ob.sum;
+    }
+}
+
+class SumDemo {
+    public static void main(String[] args) {
+        Summation s1 = new Summation(5);
+        Summation s2 = new Summation(s1);
+
+        System.out.println("s1.sum = " + s1.sum);
+        System.out.println("s2.sum = " + s2.sum);
+    }
+}
