3acec598ed3e8332d5866a7c5004a426f73a1074
1naccessible <alexpomash@gmail.com>
2022-08-27 01:54:57 +0300

    CODE

--- Queue.java
+++ Queue.java
@@ -1,13 +1,38 @@
+//Класс реализующий очередь для хранения символов
 public class Queue {
-   private int q[];// массив для хранения элементов очереди
-    private int putloc, getloc;//индексы для вставки и извлечения элементов очереди соответсвенно
+    private char q[]; //Массив для хранения элементов очереди
+    private int putloc, getloc; //Индексы для вставки и извлечения элементов очереди
 
+
+    //Сконструировать пустую очередь заданного размера
     Queue(int size) {
-        q = new int[size + 1]; //выделить память для очереди
+        q = new char[size + 1]; //Выделить память для очереди
         putloc = getloc = 0;
     }
 
-    void put(int ch) {             //поместить элемент в очердь
+    Queue(Queue ob) { //Сконструировать очередь на основе имеющегося обьекта Queue
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+
+        //Копировать элементы в очередь
+        for (int i = getloc + 1; i <= putloc; i++)
+            q[i] = ob.q[i];
+
+    }
+
+    //Сконструировать очередь на основе массива исходных значений
+    Queue(char a[]) {
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length];
+
+        for (int i = 0; i < a.length; i++)
+            put(a[i]);
+    }
+
+    //Поместить символ в очередь
+    void put(char ch) {
         if (putloc == q.length - 1) {
             System.out.println(" - Очередь заполнена");
             return;
@@ -16,10 +41,11 @@ public class Queue {
         q[putloc] = ch;
     }
 
-    int get() {    //извлечь элемент из очереди
-        if (getloc == putloc) {
+    //Извлечь символ из очереди
+    char get() {
+        if (putloc == getloc) {
             System.out.println(" - Очередь пуста");
-            return 0;
+            return (char) 0;
         }
 
         getloc++;
@@ -27,43 +53,49 @@ public class Queue {
     }
 }
 
-class QDemo {
+//Продемонстрировать использование класса Queue
+class QDemo2 {
     public static void main(String[] args) {
-        Queue bigQ = new Queue(100);
-        Queue smallQ = new Queue(4);
-        int ch;
-        int i;
+        //Создать пустую очередь для хранения 10 элементов
+        Queue q1 = new Queue(10);
 
-        System.out.println("Использование очереди bigQ для сохранения алфавита");
-        //поместить целочисленные символы в очередь bigQ
-        for (i = 0; i < 26; i++)
-            bigQ.put(i);
+        char name[] = {'T', 'o', 'm', 'h' };
+        //создать очередь на основе массива
+        Queue q2 = new Queue(name);
 
-        // извлечь буквенные символы из очереди bigQ и отобразить
-        System.out.println("Содержимое очереди bigQ");
-        for (i = 0; i < 26; i++) {
-            ch = bigQ.get();
-            if (ch !=  0) System.out.print(ch +  " ");
+        char ch;
+        char i;
+
+        //Поместить ряд символов в очередь q1
+        for (i = 0; i < 10; i++) {
+            q1.put((char) ('A' + i));
         }
+
+
+        //Создать новую очередь на основе другой
+        Queue q3 = new Queue(q1);
+
+        //Показать очереди
+        System.out.println("Содержимое q1: ");
+        for (i = 0; i < 10; i++) {
+            ch = q1.get();
+            System.out.print(ch);
+        }
+
         System.out.println("\n");
 
-        System.out.println("Использование очереди smallQ для генерации ошибок");
-        for (i = 0; i < 5; i++) {
-            System.out.println("Попытка сохранения " + ( 25 - i));
-
-            smallQ.put( (25 - i));
-
-            System.out.println();
+        System.out.println("Содержимое q2: ");
+        for (i = 0; i < 3; i++) {
+            ch = q2.get();
+            System.out.print(ch);
         }
-            System.out.println();
-            //Дополнительные ошибки при обращении к очереди smallQ
-            System.out.println("Содержимое smallQ");
-            for (i = 0; i < 5; i++) {
-                ch = smallQ.get();
-                if (ch != 0)
-                    System.out.println(ch);
 
-            }
+        System.out.println("\n");
+
+        System.out.println("Содержимое q3: ");
+        for (i = 0; i < 10; i++) {
+            ch = q3.get();
+            System.out.print(ch);
         }
     }
-
+}
---
+++ Queue2.java
@@ -0,0 +1,69 @@
+public class Queue2 {
+   private int q[];// массив для хранения элементов очереди
+    private int putloc, getloc;//индексы для вставки и извлечения элементов очереди соответсвенно
+
+    Queue2(int size) {
+        q = new int[size + 1]; //выделить память для очереди
+        putloc = getloc = 0;
+    }
+
+    void put(int ch) {             //поместить элемент в очердь
+        if (putloc == q.length - 1) {
+            System.out.println(" - Очередь заполнена");
+            return;
+        }
+        putloc++;
+        q[putloc] = ch;
+    }
+
+    int get() {    //извлечь элемент из очереди
+        if (getloc == putloc) {
+            System.out.println(" - Очередь пуста");
+            return 0;
+        }
+
+        getloc++;
+        return q[getloc];
+    }
+}
+
+class QDemo {
+    public static void main(String[] args) {
+        Queue2 bigQ = new Queue2(100);
+        Queue2 smallQ = new Queue2(4);
+        int ch;
+        int i;
+
+        System.out.println("Использование очереди bigQ для сохранения алфавита");
+        //поместить целочисленные символы в очередь bigQ
+        for (i = 0; i < 26; i++)
+            bigQ.put(i);
+
+        // извлечь буквенные символы из очереди bigQ и отобразить
+        System.out.println("Содержимое очереди bigQ");
+        for (i = 0; i < 26; i++) {
+            ch = bigQ.get();
+            if (ch !=  0) System.out.print(ch +  " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Использование очереди smallQ для генерации ошибок");
+        for (i = 0; i < 5; i++) {
+            System.out.println("Попытка сохранения " + ( 25 - i));
+
+            smallQ.put( (25 - i));
+
+            System.out.println();
+        }
+            System.out.println();
+            //Дополнительные ошибки при обращении к очереди smallQ
+            System.out.println("Содержимое smallQ");
+            for (i = 0; i < 5; i++) {
+                ch = smallQ.get();
+                if (ch != 0)
+                    System.out.println(ch);
+
+            }
+        }
+    }
+
