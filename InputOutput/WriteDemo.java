72b6cae0dc877417197cf68a44aa35931a608722
1naccessible <alexpomash@gmail.com>
2022-10-10 18:55:52 +0300

    inputOutput

---
+++ InputOutput/WriteDemo.java
@@ -0,0 +1,9 @@
+package InputOutput;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b = 'a';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
