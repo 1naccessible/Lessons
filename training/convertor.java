5c62a87defb1d37002444bbffafcb4dabaa9a287
1naccessible <alexpomash@gmail.com>
2022-10-21 19:45:43 +0300

    convertor

--- training/convertor.java
+++ training/convertor.java
@@ -1,7 +1,6 @@
 package training;
 
 class Solution {
-
     static String toCamelCase(String s) {
         int indexFrom = 0;
         String convert = null;
@@ -19,20 +18,43 @@ class Solution {
             } else break;
             indexFrom = indexOne;
         }
-
-
         s = s.replace("-", "");
+
+        indexFrom = 0;
+        convert = null;
+        for (int i = 0; i < s.length(); i++) {
+
+            int indexOne = s.indexOf("_", indexFrom);
+            if (indexOne != -1) {
+                indexOne += 1;
+                if (s.charAt(indexOne) >= 97) {
+                    char nextSymbol = s.charAt(indexOne);
+                    char neededSybol = (char) (nextSymbol - 32);
+                    convert = s.replace(nextSymbol, neededSybol);
+                    s = convert;
+                }
+            } else break;
+            indexFrom = indexOne;
+        }
         s = s.replace("_", "");
         return s;
     }
 }
-
 class SolutionDemo {
-    public static void main(String[] args) {
-        String b = "up-Wall-desert-bridge-bridge-north-Lake-Lake-Yellow-down-Yellow";
-        System.out.println(Solution.toCamelCase(b));
+            public static void main(String[] args) {
+                String s = "Ninja-river_Yellow-Ninja-Street-side-Black-left";
+                System.out.println(s);
+                System.out.println();
+                System.out.println("After conversion: ");
+                System.out.println();
+                System.out.println(Solution.toCamelCase(s));
+                s = "Ninja-river-Yellow-Ninja-Street-side-Black-left";
+                System.out.println("-------------------------------------------------------------------------------");
+                System.out.println(s);
+                System.out.println();
+                System.out.println("After conversion: ");
+                System.out.println();
+                System.out.println(Solution.toCamelCase(s));
 
-        String s = "The_Stealth_Warrior";
-        System.out.println(Solution.toCamelCase(s));
+            }
     }
-}
