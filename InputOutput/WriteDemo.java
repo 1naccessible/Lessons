07936ad72f67e716e974675c53ec7f6e5c9445f2
1naccessible <alexpomash@gmail.com>
2022-10-26 17:56:08 +0300

    code

--- InputOutput/WriteDemo.java
+++ InputOutput/WriteDemo.java
@@ -1,8 +1,10 @@
 package InputOutput;
 
+import java.io.IOException;
+
 public class WriteDemo {
-    public static void main(String[] args) {
-        int b = 'a';
+    public static void main(String[] args) throws IOException {
+        byte b[] = {'a', 'b', 'c'};
         System.out.write(b);
         System.out.write('\n');
     }
