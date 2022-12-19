02b6e0cef9fe86487ae23f6c8f5f051b5231d950
1naccessible <alexpomash@gmail.com>
2022-12-19 22:46:11 +0300

    leetcode

---
+++ training/TwoSum.java
@@ -0,0 +1,16 @@
+package training;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        int[] ind = {0, 0};
+
+        return ind;
+    }
+}
+
+class DemoTwoSum {
+    public static void main(String[] args) {
+        int[] nums = {1,1};
+        TwoSum.twoSum(nums, 6);
+    }
+}
