9aa91d5d4f7b5134653cf814ea756858f58907f3
1naccessible <alexpomash@gmail.com>
2023-01-11 03:39:54 +0300

    leetcode

---
+++ leetCode/HappyNumber.java
@@ -0,0 +1,29 @@
+package leetCode;
+
+public class HappyNumber {
+    public static boolean isHappy(int n) {
+        boolean answer = n == 1;
+        if (answer) {
+            return answer;
+        }
+        if (n < 10 && n % 2 != 0) {
+            n *= n;
+        } else if (n % 2 == 0 && n < 10){
+            return false;
+        }
+
+            String integerToString = Integer.toString(n);
+        char[] buff = integerToString.toCharArray();
+        int isHappyNumber = 0;
+        for (char ch : buff) {
+            String s = "" + ch;
+            isHappyNumber += Math.pow(Integer.parseInt(s), 2);
+        }
+
+        return isHappy(isHappyNumber);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HappyNumber.isHappy(4));
+    }
+}
---
+++ leetCode/PrimeNum.java
@@ -0,0 +1,24 @@
+package leetCode;
+
+import java.util.*;
+
+public class PrimeNum {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> searchDup = new HashSet<>();
+        for (int x:
+             nums) {
+            searchDup.add(x);
+        }
+        if (searchDup.size() < nums.length){
+            return true;
+        }else return false;
+    }
+}
+
+class PrimeDemo{
+    public static void main(String[] args) {
+        PrimeNum ob = new PrimeNum();
+        int [] nums = {1,2,3,4,5};
+        System.out.println(ob.containsDuplicate(nums));
+    }
+}
