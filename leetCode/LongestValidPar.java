7da661486c6104f329a8c98b32cf94e0254b7dd2
1naccessible <alexpomash@gmail.com>
2023-01-29 01:52:36 +0300

    code

--- leetCode/LongestValidPar.java
+++ leetCode/LongestValidPar.java
@@ -3,8 +3,23 @@ package leetCode;
 import java.util.Stack;
 
 public class LongestValidPar {
-}
+    public int longestValidParentheses(String s) {
 
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    return s.length();
+                }
+            }
+        }
+        return s.length() - stack.size();
+    }
+}
 class DemoLVP {
     public static void main(String[] args) {
     }
